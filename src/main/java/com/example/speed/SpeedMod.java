aa9e3fda98bc9c5694fb64bfe30a116837d25c65
Kiserexser <towartors@mail.ru>
2026-06-08 13:23:24 +0300

    Update SpeedMod.java

--- src/main/java/com/example/speed/SpeedMod.java
+++ src/main/java/com/example/speed/SpeedMod.java
@@ -6,9 +6,15 @@ import org.slf4j.LoggerFactory;
 
 public class SpeedMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("speedmod");
+    public static boolean enabled = false;
+    public static int ticks = 0;
+    public static int groundTicks = 0;
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Speed Mod is running!");
+        LOGGER.info("Speed Mod with all bypasses loaded");
+        // Регистрация клавиши R и ивентов будет позже, пока просто заглушка
     }
+
+    public static boolean isEnabled() { return enabled; }
 }
