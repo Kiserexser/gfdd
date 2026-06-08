46b7e44d00a8232ded7d67d1521420014bc03092
Kiserexser <towartors@mail.ru>
2026-06-08 14:00:33 +0300

    Update MixinTimer.java

--- src/main/java/com/example/speed/mixin/MixinTimer.java
+++ src/main/java/com/example/speed/mixin/MixinTimer.java
@@ -3,16 +3,17 @@ package com.example.speed.mixin;
 import com.example.speed.TimerManager;
 import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderTickCounter.class)
 public class MixinTimer {
-    @Shadow private float tickTime;
+    @Unique
+    private float tickTime;
 
-    @Inject(method = "beginRenderTick", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "beginRenderTick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/RenderTickCounter;prevTimeMillis:J", opcode = 0xb4), cancellable = true)
     private void onBeginRenderTick(long nanos, CallbackInfoReturnable<Integer> cir) {
         float multiplier = TimerManager.getTimer();
         if (multiplier != 1.0F) {
