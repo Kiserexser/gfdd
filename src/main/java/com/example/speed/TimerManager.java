53ac4453e941034f6d5f53121ea55d153c931406
Kiserexser <towartors@mail.ru>
2026-06-08 14:30:50 +0300

    Delete src/main/java/com/example/speed/TimerManager.java

--- src/main/java/com/example/speed/TimerManager.java
+++
@@ -1,7 +0,0 @@
-package com.example.speed;
-
-public class TimerManager {
-    private static float clientTickMultiplier = 1.0F;
-    public static void setTimer(float speed) { clientTickMultiplier = speed; }
-    public static float getTimer() { return clientTickMultiplier; }
-}
