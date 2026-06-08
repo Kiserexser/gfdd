472e10a6f93ad9fe752bf7c2bd84402b6f308c69
Kiserexser <towartors@mail.ru>
2026-06-08 13:35:01 +0300

    Update SpeedMod.java

--- src/main/java/com/example/speed/SpeedMod.java
+++ src/main/java/com/example/speed/SpeedMod.java
@@ -1,11 +1,6 @@
 package com.example.speed;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,22 +9,7 @@ public class SpeedMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Speed Mod with all bypasses loaded");
-
-        // Регистрация клавиши R
-        KeyBinding key = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.speed.toggle",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
-                "category.speed"
-        ));
-
-        // Обработчик нажатий (каждый тик проверяем, нажата ли клавиша)
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (key.wasPressed()) {
-                // Переключаем состояние модуля
-                EventManager.setEnabled(!EventManager.isEnabled());
-            }
-        });
+        LOGGER.info("Speed Mod loaded without Fabric API");
+        // Клавиша будет зарегистрирована и обработана в миксинах
     }
 }
